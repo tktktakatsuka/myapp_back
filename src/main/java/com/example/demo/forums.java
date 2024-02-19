54abe3a54e4dbdd38b32224a438bda80386aa70f
tktktakatuska <blue.mountain.syota@icloud.com>
2024-02-20 07:54:28 +0900

    hoge

--- src/main/java/com/example/demo/forums.java
+++ src/main/java/com/example/demo/forums.java
@@ -1,7 +1,7 @@
 package com.example.demo;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 import lombok.Data;
 
